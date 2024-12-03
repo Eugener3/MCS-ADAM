dbc6a88b2d6a1c3109336e43a6cfedafa94c1830
Eugene <musi-masi@mail.ru>
2024-12-03 14:03:06 +0300

    fix(telegram): field key to list dto

--- src/telegram/dto/list.dto.ts
+++ src/telegram/dto/list.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { AuthDto } from "src/common/dto/auth.dto";
 
-export class TelegramListDto {
+export class TelegramListDto extends AuthDto {
     @ApiProperty({
         default: false,
         description: 'Whether to subscribe to the broadcast messages',
